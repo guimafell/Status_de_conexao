35a8224f30c6d2228cc96d2c49d6724c742ff968
guimafell <60404761+guimafell@users.noreply.github.com>
2021-02-01 22:56:40 -0300

    UP
    Este é o verificador.
    tis is the verificator.

---
+++ Inicio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace conexao_internet
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+        [DllImport("wininet.dll")]
+        private extern static Boolean InternetGetConnectedState(out int Description, int ReservedValue);
+
+        public static Boolean Isconnected()
+        {
+            int Description;
+            return InternetGetConnectedState(out Description, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Isconnected())
+            {
+                textBox1.Text = "Conectado";
+            }
+            else
+            {
+                textBox1.Text = "Desconectado";
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+Este é um programa simples que avalia a sua conexão
+com a internet dizendo o status(conectado ou desconectado).
+Até a proxima :)
+
+This is a simple program that evaluates your connection
+with the internet saying the status (connected or disconnected).
+See you later :)
